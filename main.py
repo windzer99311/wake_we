5c000f3a60eb3fae9bd79a07133c47540f689d2f
windzer99311 <windzergamer@gmail.com>
2025-06-16 18:02:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,12 @@ def wake_web():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1200')
+    url=r'https://visitcount-ymbj8jwzkdo4rdnrnspkqm.streamlit.app/'
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                               options=options)
     st.write(f"DEBUG:DRIVER:{driver}")
-    while True:
-        time.sleep(30)
-        log_lines = []
-
-        try:
-            with open('weblist.txt', 'r', encoding='utf-8') as f:
-                urls = [line.strip() for line in f if line.strip()]
-                for url in urls:
-                    try:
-                        driver.get(url)
-                        log_line = f" ✅ {url} → 200"
-                    except:
-                        log_line = f" ❌ {url} → Error"
-                    print(log_line)
-                    log_lines.append(log_line)
-        except FileNotFoundError:
-            log_lines.append(f" ❌ weblist.txt not found.")
+    driver.get(url)
+    time.sleep(5)
+    html_doc = driver.page_source
+    driver.quit()
 wake_web()
