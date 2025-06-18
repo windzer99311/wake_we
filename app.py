048f72778e1a8643198daf9c63b5354626b71252
windzer99311 <windzergamer@gmail.com>
2025-06-18 21:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def wake_web():
                 for line in log_lines:
                     f.write(line + "\n")
 
-        time.sleep(30) 
+        time.sleep(300) 
 
 # Start background thread only once
 if not hasattr(st, "_wake_thread_started"):
